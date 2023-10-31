b52dbfc3203e2f547fde3e612a02dd471deb213e
tantohtelmah <tantohtelmah@gmail.com>
2023-10-30 21:00:44 -0400

    checks

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
-print(str)
+print(f"{str[39:66]}{str[-23:-17]}{str[:6]}")
