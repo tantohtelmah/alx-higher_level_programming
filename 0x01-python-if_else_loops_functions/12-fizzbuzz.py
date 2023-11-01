4f37414c48e31ca4f8cec473fe66398c0ae57cd2
tantohtelmah <tantohtelmah@gmail.com>
2023-10-31 22:57:46 -0400

    task100

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     result = ""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            result += "FizzBuzz "
+            result += "FizzBuzz"
         elif i % 3 == 0:
             result += "Fizz "
         elif i % 5 == 0:
