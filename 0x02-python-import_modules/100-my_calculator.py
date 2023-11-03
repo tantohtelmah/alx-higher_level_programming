f95e5fef45f9bad5013f67d0e6b23ee37f177c6b
tantohtelmah <tantohtelmah@gmail.com>
2023-11-02 23:02:11 -0400

    task100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calc
+    
+    lenght = len(sys.argv)
+    if lenght != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    
+    if sys.argv[2] not in ["+", "*", "-", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    a = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    if sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    if sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    if sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, calc.div(a, b)))
+        
