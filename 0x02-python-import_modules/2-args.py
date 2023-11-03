c884b5faa0d562150162a6900fb3ebf51e622bd0
tantohtelmah <tantohtelmah@gmail.com>
2023-11-02 21:35:17 -0400

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-lenght = len(sys.argv)
-if lenght == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(lenght - 1))
-    for i in range(lenght - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    lenght = len(sys.argv)
+    if lenght == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(lenght - 1))
+        for i in range(lenght - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
