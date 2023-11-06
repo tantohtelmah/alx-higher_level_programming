ae52088c78e9d721256a99f92c773490e1d033b2
tantohtelmah <tantohtelmah@gmail.com>
2023-11-05 20:09:58 -0500

    task0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if i <= len(my_list):
-            print("{}".format(my_list[i - 1]))
+        if isinstance(i, int):
+            if i <= len(my_list):
+                print("{}".format(my_list[i - 1]))
+            else:
+                print("Index out of range")
         else:
-            print("Index out of range")
+            print("Non interger")
