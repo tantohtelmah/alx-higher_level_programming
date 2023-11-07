68dbf47fe0860fc3955e2a3348f0a58b430ea8ab
tantohtelmah <tantohtelmah@gmail.com>
2023-11-06 21:46:47 -0500

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-    from not_int import is_not_integer
-    
-lenght = len(sys.argv)
-result = 0
-if lenght == 1:
-    print("0")
-else:
-    for i in range(1, lenght):
-        for arg in sys.argv[1, lenght]:
-            if is_not_integer(arg):
-                print("{} is not an integer".format(arg), end="")
-        result += int(sys.argv[i])
-    print("{}".format(result))
+
+def add_all_args():
+    args = sys.argv[1:]
+    total = sum(int(arg) for arg in args)
+    print(total)
+
+if __name__ == '__main__':
+    add_all_args()
