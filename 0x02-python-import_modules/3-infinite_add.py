346237be2e923b0c61a39787089dea6e1311d0e0
tantohtelmah <tantohtelmah@gmail.com>
2023-11-06 21:49:05 -0500

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def add_all_args():
     args = sys.argv[1:]
     total = sum(int(arg) for arg in args)
     print(total)
+
 
 if __name__ == '__main__':
     add_all_args()
