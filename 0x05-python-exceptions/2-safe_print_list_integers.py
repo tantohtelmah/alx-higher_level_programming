e6c80bdb72f56425405d091e5d24c90b450c191c
tantohtelmah <tantohtelmah@gmail.com>
2023-11-20 21:34:04 -0500

    task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
                 if count < x:
                     print(" ", end="\n")
-        except:
+        except ValueError:
             pass
     print()
     return count
