b2a15a86ca2f2b6d535629ce6662c83f320aa92f
tantohtelmah <tantohtelmah@gmail.com>
2023-11-20 20:38:12 -0500

    task3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             if count < x:
                 print("{}".format(i), end="")
                 count += 1
-        except:
+        except ValueError:
             pass
     print()
     return count
