05c87aebcfe12fa2da0218da8826daf7a6eb7cdf
tantohtelmah <tantohtelmah@gmail.com>
2023-11-21 20:45:40 -0500

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,5 +8,6 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
+
     def __init__(self, size=0):
         self.__size = self.validator(size)
