1ccd4f20f81c19312c2a3fe15ceb048bc9566b4f
tantohtelmah <tantohtelmah@gmail.com>
2023-11-21 21:13:11 -0500

    task0

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" def of square class """
+
+
+class Square:
+    """ Implementation """
+
+    def validator(self, value):
+        if type(value) is int:
+            if value >= 0:
+                return value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def __init__(self, size=0):
+        self.__size = self.validator(size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = self.validator(value)
+
+    def area(self):
+        return self.size * self.size
+    
