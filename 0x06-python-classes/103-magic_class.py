f284661dc7e67ab7bd14afaa20e6cab0affa6e12
tantohtelmah <tantohtelmah@gmail.com>
2023-11-22 21:46:58 -0500

    task0

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import dis
+
+class MyClass:
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        return self.radius ** 2 * 3.14159
+
+    def circumference(self):
+        return 2 * self.radius * 3.14159
+
+dis.dis(MyClass.__init__)
