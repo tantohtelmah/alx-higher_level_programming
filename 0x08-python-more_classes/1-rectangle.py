7ad20f4312c32343cd3187a02631b4f63bb61a13
tantohtelmah <tantohtelmah@gmail.com>
2023-11-27 21:52:48 -0500

    task101

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,7 @@
 class Rectangle:
     """ initialisation """
 
-    def __init__(self, height=0, width=0):
+    def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
 
