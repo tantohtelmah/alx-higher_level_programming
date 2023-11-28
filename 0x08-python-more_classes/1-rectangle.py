3685563a499f9bcbbaa76aafe2eb8fe770c79a0b
tantohtelmah <tantohtelmah@gmail.com>
2023-11-27 21:44:51 -0500

    task101

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 class Rectangle:
     """ Initialising """
-    
-    
+
     def width_validator(self, value):
-            if type(value) is int:
-                if value >= 0:
-                    return value
-                else:
-                    raise ValueError("width must be >= 0")
+        if type(value) is int:
+            if value >= 0:
+                return value
             else:
-                raise TypeError("width must be an integer")
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
     def height_validator(self, value):
-            if type(value) is int:
-                if value >= 0:
-                    return value
-                else:
-                    raise ValueError("height must be >= 0")
+        if type(value) is int:
+            if value >= 0:
+                return value
             else:
-                raise TypeError("height must be an integer")
-            
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
     def __init__(self, width=0, height=0):
         self.__width = self.width_validator(width)
         self.__height = self.height_validator(height)
-        
+
         @property
         def width(self):
             return self.__width
@@ -31,7 +31,7 @@ class Rectangle:
         @width.setter
         def width(self, value):
             self.__width = self
-            
+
         @property
         def height(self):
             return self.__height
