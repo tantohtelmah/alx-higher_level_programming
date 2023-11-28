58119fcd1e4eb3b4dc8baea8c259a9f3c25991a2
tantohtelmah <tantohtelmah@gmail.com>
2023-11-27 21:26:04 -0500

    task101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,15 +4,17 @@ import sys
 
 
 def nqueens(n):
-    """ Solves the N queens problem """
-    
-    
+    """
+    Solves the N queens problem
+    """
+
     if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
+
     def solve(queens, xy_dif, xy_sum):
         p = len(queens)
         if p == n:
@@ -22,9 +24,10 @@ def nqueens(n):
             if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
                 solve(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
     result = []
-    solve([],[],[])
+    solve([], [], [])
     for solution in result:
         print(' '.join(['#' * i + 'Q' + '#' * (n-i-1) for i in solution]))
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
