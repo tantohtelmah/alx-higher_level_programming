411a8b3fcd966e15547ae6e039772fed254a25b8
tantohtelmah <tantohtelmah@gmail.com>
2023-12-04 21:07:49 -0500

    task 101

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""  inherits from int """
+""" initialisation """
 
 
 class MyInt(int):
     """ the class """
-    
+
     def __eq__(self, other):
         """ function """
-        
+
         return int(self) != int(other)
 
     def __ne__(self, other):
-        """ function """
+        """function"""
+
         return int(self) == int(other)
