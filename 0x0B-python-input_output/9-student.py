53a39e6c73e5df710086766bf4dd5cd738de48b8
tantohtelmah <tantohtelmah@gmail.com>
2023-12-05 21:34:46 -0500

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,9 +8,9 @@ class Student:
     """ Class Student"""
 
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def class_to_json(self):
         """Returns the dictionary
