e8e9ad3952db5084082bb407f5f563886f4ec9ee
tantohtelmah <tantohtelmah@gmail.com>
2023-12-05 21:22:03 -0500

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,19 +6,23 @@ import sys
 import json
 from typing import List
 
+
 def save_to_json_file(my_obj: List, filename: str) -> None:
 
     """Saves an object to a JSON file."""
     with open(filename, mode='w', encoding='utf-8') as f:
         json.dump(my_obj, f)
 
-def load_from_json_file(filename: str) -> List:
 
+def load_from_json_file(filename: str) -> List:
     """Loads an object from a JSON file."""
+
     with open(filename, mode='r', encoding='utf-8') as f:
         return json.load(f)
 
+
 if __name__ == '__main__':
+
     filename = 'add_item.json'
     try:
         my_list = load_from_json_file(filename)
