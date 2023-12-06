a9c1ab628a7fbe22df6bfb1e59d1a040b910f523
tantohtelmah <tantohtelmah@gmail.com>
2023-12-05 21:59:21 -0500

    test

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,7 +6,9 @@ class Student:
     """Defines a student by first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Instantiates a Student object with first_name, last_name, and age."""
+        """Instantiates a Student object with first_name,
+        last_name, and age.
+        """
 
         self.first_name = first_name
         self.last_name = last_name
@@ -18,7 +20,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
 
     def reload_from_json(self, json):
         """Replaces all attributes of the Student instance."""
