99a8967e94e4eff01c40063614c48d14ea11b295
tantohtelmah <tantohtelmah@gmail.com>
2023-12-05 21:38:38 -0500

    test

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,18 +3,18 @@
 initialisation
 """
 
+
 class Student:
     """ Class Student"""
-    
+
     def __init__(self, first_name, last_name, age):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__age = age
-        
+
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance."""
         if attrs is None:
             return self.__dict__
         else:
             return {k: v for k, v in self.__dict__.items() if k in attrs}
-        
