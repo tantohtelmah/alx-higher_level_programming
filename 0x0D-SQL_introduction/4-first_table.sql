b2e91e1d246baad288d34f7b948fb20e169cc67d
tantohtelmah <tantohtelmah@gmail.com>
2024-01-16 20:58:34 -0500

    tasks

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Create a table called first_table with columns id and name
+-- If the table first_table already exists, the script should not fail
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
