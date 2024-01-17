86f8381c05481db52f56218adc169baaf983c4e0
tantohtelmah <tantohtelmah@gmail.com>
2024-01-16 20:59:37 -0500

    tasks

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Print the full description of the table first_table from the database hbtn_0c_0
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
