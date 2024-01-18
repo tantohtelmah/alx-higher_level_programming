a48678088493103a4b6467f447a7e4912d55644a
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 20:23:28 -0500

    tasks

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create the specified user with all privileges
 SET @password = 'user_0d_1_pwd';
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY @password;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
