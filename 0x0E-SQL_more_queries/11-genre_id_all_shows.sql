c76f60104366513294837dc38e7c363f80d43ce7
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 21:33:50 -0500

    tasks

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
--- Listing all shows contained in database
+-- lists shows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
