fd36160a7ebbf367f83b77181d7586d6f666dede
tantohtelmah <tantohtelmah@gmail.com>
2024-01-18 00:09:59 -0500

    tasks

--- 0x0D-SQL_introduction/103-rating_genres.sql
+++
@@ -1,7 +0,0 @@
--- lists all genres in the database database by their rating, sorted in descending order by their rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS rating_sum
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.id
-ORDER BY rating_sum DESC;
