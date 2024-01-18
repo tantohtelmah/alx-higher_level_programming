548b3e9cd908ec132e682a828a0cd17ab3d132d3
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 23:56:39 -0500

    tasks

---
+++ 0x0D-SQL_introduction/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- list all shows from database by their rating
+SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
+FROM tv_shows
+JOIN hbtn_0d_tvshows_rate ON tv_shows.id = hbtn_0d_tvshows_rate.tv_show_id
+GROUP BY tv_shows.id
+ORDER BY rating_sum DESC;
