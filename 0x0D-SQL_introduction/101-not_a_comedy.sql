2709de1b9d39497a3a995f4b32477f4d84484aa4
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 23:37:12 -0500

    tasks

---
+++ 0x0D-SQL_introduction/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Lists all shows without "comedy"
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.id NOT IN (
+    SELECT tv_shows.id
+    FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
--- List all tables in a MySQL database
-USE mysql
+-- List all the tables in the specified database
 SHOW TABLES;
