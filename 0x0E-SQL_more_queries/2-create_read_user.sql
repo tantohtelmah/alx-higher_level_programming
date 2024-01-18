68c48927d369b80b85764a70505175fc2535412b
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 20:27:27 -0500

    tasks

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
--- Create the specified database if it does not exist
-SET @password = 'user_0d_2_pwd';
+-- Create the specified user with all privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
