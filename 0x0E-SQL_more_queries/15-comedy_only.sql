1822db9101798891ba64d3a384f19feb8379cc8b
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 21:45:14 -0500

    tasks

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- ists all Comedy shows in the database
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title ASC;
