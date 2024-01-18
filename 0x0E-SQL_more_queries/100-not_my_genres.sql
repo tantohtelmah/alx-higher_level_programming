5e58bee9a4dcc98b0bad5a9baeb64579df28d769
tantohtelmah <tantohtelmah@gmail.com>
2024-01-17 21:57:38 -0500

    tasks

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- lists
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+  SELECT genre_id
+  FROM tv_show_genres
+  WHERE show_id = (
+    SELECT id
+    FROM tv_shows
+    WHERE title = 'Dexter'
+  )
+)
+ORDER BY name ASC;
