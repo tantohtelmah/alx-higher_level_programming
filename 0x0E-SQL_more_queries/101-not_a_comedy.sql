0da68db51e9d02202b10409839e7926cfcb77980
tantohtelmah <tantohtelmah@gmail.com>
2024-01-18 21:58:23 -0500

    task

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,10 +3,10 @@ SELECT tv_shows.title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.id NOT IN (
-    SELECT tv_shows.id
+WHERE shows.id NOT IN (
+    SELECT shows.id
     FROM tv_shows
-    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_show_genres ON shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy'
 )
