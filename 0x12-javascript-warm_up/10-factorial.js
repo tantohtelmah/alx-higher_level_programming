5da0fc3ee7c1be0dd0e5867e70373e79282ddf6d
tantohtelmah <tantohtelmah@gmail.com>
2024-02-12 21:44:27 -0500

    task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+function factorial(n) {
+	if (n < 0) {
+	  return; // Factorial of negative numbers does not exist
+	}
+  
+	if (n === 0) {
+	  return 1; // Factorial of 0 is 1
+	}
+  
+	return n * factorial(n - 1);
+  }
+  
+  // Example usage:
+  const num = parseInt(process.argv[2]);
+  if (!isNaN(num)) {
+	const result = factorial(num);
+	console.log(`The factorial of ${num} is: ${result}`);
+  } else {
+	console.log("Missing number of occurrences");
+  }
+  
