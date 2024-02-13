acafcc0f148bf54878cacab6f1b708adc8702f07
tantohtelmah <tantohtelmah@gmail.com>
2024-02-12 21:45:34 -0500

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+function findSecondLargest() {
+	const args = process.argv.slice(2); // Exclude the first two arguments (node and script name)
+	const integers = args.map(Number).filter(Number.isInteger);
+  
+	if (integers.length === 0) {
+	  console.log(0);
+	} else if (integers.length === 1) {
+	  console.log(0);
+	} else {
+	  integers.sort((a, b) => b - a); // Sort in descending order
+	  console.log(integers[1]);
+	}
+  }
+  
+  findSecondLargest();
+  
