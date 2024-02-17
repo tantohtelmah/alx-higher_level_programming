51a23deaefbf5938cf7fb9ae5de925fa166f766f
tantohtelmah <tantohtelmah@gmail.com>
2024-02-17 00:15:44 -0500

    tasks

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Initiation of the code
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Get MySQL credentials from command line arguments
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Create an SQLite engine
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all State objects and sort by id
+    states = session.query(State).order_by(State.id).all()
+
+    # Display results
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
