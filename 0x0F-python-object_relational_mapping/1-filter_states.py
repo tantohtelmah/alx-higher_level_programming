5abd4e6330cc6888f870633545bde09f303a50a0
tantohtelmah <tantohtelmah@gmail.com>
2024-02-18 21:28:51 -0500

    tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -24,8 +24,7 @@ if __name__ == "__main__":
         cursor = db.cursor()
 
         # Execute SQL query to retrieve states starting with 'N'
-        cursor.execute("SELECT * FROM states WHERE name LIKE \
-            'N%' ORDER BY id ASC")
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
         states = cursor.fetchall()
 
         # Display results
