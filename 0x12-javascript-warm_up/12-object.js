9b3ce34c62aa62f023abbfd76e1c9b143678a443
tantohtelmah <tantohtelmah@gmail.com>
2024-03-11 20:52:30 -0400

    task

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Updating the code to change a value within an object
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// MY CODE HERE
+myObject.value = 89;
+
+console.log(myObject);
