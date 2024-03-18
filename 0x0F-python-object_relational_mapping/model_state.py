e344c5f9390b76ed789556eceb7405ce8092db80
tantohtelmah <tantohtelmah@gmail.com>
2024-03-17 22:23:43 -0400

    tasks

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 if __name__ == "__main__":
     class State(Base):
         """
