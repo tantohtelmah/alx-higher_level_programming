6eb8e1444e9b3a2f8bcb0daa3b487ed979a07554
tantohtelmah <tantohtelmah@gmail.com>
2024-03-17 22:13:54 -0400

    tasks

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -17,5 +17,5 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
