3c81201c7d15e357c65aabf16041c87965af0263
tantohtelmah <tantohtelmah@gmail.com>
2024-03-27 21:50:21 -0400

    tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Variables for the POST request
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send a POST request using curl, include the variables, and store the response in a temporary file
-response_file=$(mktemp)
-curl -s -X POST -d "email=$email&subject=$subject" "$url" > "$response_file"
-
-# Display the body of the response
-cat "$response_file"
-
-# Clean up the temporary file
-rm "$response_file"
+# sends a POST request to the passed URL, and displays the body
+curl -s -X POST -d "email=test@gmail.com&subject=$I will always be here for PLD" "$1"
