976607a29b2c2ae4c13812a01152c95e6bcfa082
tantohtelmah <tantohtelmah@gmail.com>
2024-03-27 21:54:27 -0400

    tasks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Send a request using curl and capture the status code
-http_status=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-# Display the status code
-echo "HTTP status code: $http_status"
+#  sends a request to a URL passed as an argument
+curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# Check if both URL and filename arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <URL> <filename>"
-    exit 1
-fi
-
-# Get the URL and filename from the command line arguments
-url="$1"
-filename="$2"
-
-# Read the contents of the file
-json_data=$(cat "$filename")
-
-# Send a POST request using curl, include the JSON data, and store the response in a temporary file
-response_file=$(mktemp)
-curl -s -X POST -H "Content-Type: application/json" -d "$json_data" "$url" > "$response_file"
-
-# Display the body of the response
-cat "$response_file"
-
-# Clean up the temporary file
-rm "$response_file"
+# sends a JSON POST request to the specified URL using curl
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
