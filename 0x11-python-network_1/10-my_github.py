7963183bbf75e372ff88f98e9779e28f8d7a075c
tantohtelmah <tantohtelmah@gmail.com>
2024-03-28 23:00:24 -0400

    tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,32 +5,28 @@ Inisialisation of the code
 import requests
 import sys
 
-
-def get_github_user_id(username, token):
-    url = "https://api.github.com/user"
-    headers = {"Authorization": f"Basic {username}:{token}"}
-
+def get_user_id(username, token):
     try:
+        url = f"https://api.github.com/user"
+        headers = {
+            "Authorization": f"Basic {username}:{token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
         response = requests.get(url, headers=headers)
-        # Raise an exception if the status code is not 200 (OK)
-        response.raise_for_status()
-
-        user_data = response.json()
-        user_id = user_data.get("id")
-        if user_id:
-            print(f"Your GitHub user ID: {user_id}")
+        if response.status_code == 200:
+            user_data = response.json()
+            user_id = user_data.get("id")
+            print(f"Your GitHub user ID is: {user_id}")
         else:
-            print("Unable to retrieve user ID.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data from GitHub API: {e}")
+            print(f"Error fetching user data. Status code: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print(
-            "Usage: python script_name.py <username> <personal_access_token>")
+        print("Usage: python script.py <username> <personal_access_token>")
         sys.exit(1)
 
-    username = sys.argv[1]
-    token = sys.argv[2]
-    get_github_user_id(username, token)
+    username, token = sys.argv[1], sys.argv[2]
+    get_user_id(username, token)
