6d4bc6200c52e0513f539d985cef8b0c21876aee
tantohtelmah <tantohtelmah@gmail.com>
2024-04-23 21:25:23 -0400

    tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function computeCompletedTasksByUser (url) {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(`${error.message}`);
+    } else if (response.statusCode === 200) {
+      const todos = JSON.parse(body);
+      const completedTasksByUser = {};
+
+      for (const todo of todos) {
+        if (todo.completed) {
+          const userId = todo.userId;
+          completedTasksByUser[userId] = completedTasksByUser[userId] || 0;
+          completedTasksByUser[userId]++;
+        }
+      }
+	  console.log(completedTasksByUser);
+    } else {
+      console.error(`${response.statusCode}`);
+    }
+  });
+}
+const url = process.argv[2];
+computeCompletedTasksByUser(url);
