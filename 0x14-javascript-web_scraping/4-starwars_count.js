621e13f0df0201242554ae92455de269af36e5df
tantohtelmah <tantohtelmah@gmail.com>
2024-04-23 22:31:11 -0400

    tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-function getMovieCountForCharacter (apiUrl) {
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`${error.message}`);
-    } else if (response.statusCode === 200) {
-      const movies = JSON.parse(body).results;
-      const filteredMovies = movies.filter(movie =>
-        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-      );
-      console.log(`${filteredMovies.length}`);
-    } else {
-      console.error(`Error fetching data. Status code: ${response.statusCode}`);
-    }
-  });
-}
-
-const url = process.argv[2];
-getMovieCountForCharacter(url);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
